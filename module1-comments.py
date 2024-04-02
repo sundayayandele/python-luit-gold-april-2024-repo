6624622a35dc6fab84c9fa0cb63f2d597b8e60e0
sabahatk <sabahatkhan@live.ca>
2024-04-02 23:20:58 +0000

    Week 13 Comments Lesson

---
+++ module1-comments.py
@@ -0,0 +1,9 @@
+print('sample') #Here is a mid-line comment
+#Here is an all-line comment
+
+#Note that the varaibles are strings, not numbers!
+var1 = '23'
+var2 = '54'
+
+print('hello')
+print('hidden message')
