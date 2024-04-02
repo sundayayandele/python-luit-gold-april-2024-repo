2795c3ce479f5398883aaf14d8b3b588cfa54fe5
sabahatk <sabahatkhan@live.ca>
2024-04-02 23:18:04 +0000

    Week 13 Data Types Lesson

---
+++ module1-data-types.py
@@ -0,0 +1,10 @@
+greeting = 'Hello, friend!' #String
+
+age = 35 #Integer
+
+speed1 = 4.5 # Float
+speed2 = 4.0
+speed3 = 4.
+speed4 = 4 # Integer
+
+bool_test = False #Boolean, must have capital first letter, can be 1 or 0
