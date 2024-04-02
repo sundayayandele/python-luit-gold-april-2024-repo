2c347f39102832c4a24f89771fc8c957f4aa8a65
sabahatk <sabahatkhan@live.ca>
2024-04-02 23:33:16 +0000

    Week 13 Numerical Rep Lesson

---
+++ module1-numerical-representations.py
@@ -0,0 +1,16 @@
+#underscores in numbers
+num1 = 12000300
+num2 = 12_000_300
+
+#scientific notation
+# 3e4 = 3E4 = 3 * 10000 = 30000
+#3e-4 = 3E-4 = 3 * 1/10000 = 0.0003
+print(0.000000000000000000000000003)
+
+#octal numbers
+#start with 00 or 0o
+print(0o123)
+
+#hexadecimal numbers
+#start with 0X or ox
+print(0x123)
