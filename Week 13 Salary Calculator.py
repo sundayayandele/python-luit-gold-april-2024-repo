54a6901ceb0a061036c5969d0d7b4ef41bf82bb2
sabahatk <sabahatkhan@live.ca>
2024-04-03 23:33:56 +0000

    Week 13 Salary Calc Exercise

---
+++ Week 13 Salary Calculator.py
@@ -0,0 +1,4 @@
+hours = float(input("How many hours did you work last month? "))
+hourly_rate = float(input("What is your hourly rate? "))
+dollars = hours * hourly_rate
+print("Last month, you earned " + str(dollars) + " dollars")
