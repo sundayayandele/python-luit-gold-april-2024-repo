92025d2a0b293f53eefc42f9e9ca4ae521d27f7f
sabahatk <sabahatkhan@live.ca>
2024-04-03 01:20:04 +0000

    Week 13 Reassigning Variables Lesson

---
+++ module1-reassigning-values.py
@@ -0,0 +1,22 @@
+age = 28
+print(age)
+
+age = 28
+new_age = age + 5
+print(new_age)
+
+age = 28
+age += 7
+print(age)
+
+age *= 2
+age -= 5
+age /= 3
+
+text = 'hokus' + 'pokus'
+print(text)
+
+print('hokus' * 5)
+
+print(23+5)
+print('23' + '5')
