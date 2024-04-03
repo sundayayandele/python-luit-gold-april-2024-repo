6d268993d0a384a551d35eabf31e05b96b19cc45
sabahatk <sabahatkhan@live.ca>
2024-04-03 01:13:47 +0000

    Week 13 Operators Lesson

---
+++ module1-operators.py
@@ -0,0 +1,24 @@
+nunmSum = 2 + 3 #5
+numDiff = 5 - 4 #1
+numProduct = 3 * 5 #15
+#standard division
+numDiv = 6/2 #3
+numDiv2 = 7/2 #3.5
+
+#integer division
+numIntDiv = 6//2 #3
+numIntDiv2 = 7//2 #3
+
+#modulus division
+numModDiv = 6%2 #0
+numModdiv = 7%2 #1
+
+#Error
+numErr = 5/0 #ZeroDivisionError
+
+#power operator
+numPow = 2 ** 3 # 2^3 = 8
+
+numIntSum = 2 + 3 # 5
+numFloatSum = 2+3.0 # 5.0
+numFloatSum2 = 2.0+3.0 # 5.0
