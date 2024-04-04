d1f5264ff4a7e87ecfe36fd5fc14cb5b2a53c790
sabahatk <sabahatkhan@live.ca>
2024-04-04 01:24:47 +0000

    Week 13 Bit Operator Lesson

---
+++ module2-bit-operators.py
@@ -0,0 +1,22 @@
+#Bitwise Operators: &, |, ~, ^, <<, >>
+
+first_bit = 1
+second_bit = 1
+
+print(first_bit & second_bit) #1
+
+first_bit = 1
+second_bit = 0
+
+print(first_bit | second_bit) #1
+
+first_bit = 0
+second_bit = 0
+print(first_bit ^ second_bit) #1
+
+print(~0)
+print(12 << 1)
+print(12 << 2)
+print(12 << 3)
+print(12 >> 1)
+print(12 >> 2)
