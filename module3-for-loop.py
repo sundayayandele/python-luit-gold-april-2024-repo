d9d88e0c589465bb7c4b3347dc82ff91b2ebf522
sabahatk <sabahatkhan@live.ca>
2024-04-05 20:07:49 +0000

    Week 13 For Loop Lesson

---
+++ module3-for-loop.py
@@ -0,0 +1,21 @@
+for letter in "hello!":
+    print("Current letter:", letter)
+    
+counter = 1
+while counter < 11:
+    print(counter)
+    counter += 1
+print("Finished!")
+
+#Three types of range:
+for counter in range(1,11):
+    print(counter)
+print("Finished!")
+
+for counter in range(11):
+    print(counter)
+print("Finished!")
+
+for counter in range(1,11, 2):
+    print(counter)
+print("Finished!")
