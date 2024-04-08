c045757d1ffc3f7862242a927ef312c1d6544b3e
sabahatk <sabahatkhan@live.ca>
2024-04-08 22:21:50 +0000

    Week 14 Intro List Lesson

---
+++ module4-intro-lists.py
@@ -0,0 +1,25 @@
+#Lists
+city_1 = "New York City"
+city_2 = "Los Angeles"
+city_3 = "Chicago"
+city_4 = "Houston"
+city_5 = "Phoenix"
+
+empty_list = []
+
+top_cities = ["New York City", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+print(top_cities)
+
+print(top_cities[0])
+
+#print first 2 elements
+print(top_cities[0:2])
+#print from third element onwards
+print(top_cities[2:])
+# print all until the third element
+print(top_cities[:3])
+#print entire list
+print(top_cities[:])
+print(top_cities)
+
+
