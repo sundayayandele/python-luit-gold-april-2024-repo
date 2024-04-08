3f61969fc05a1dcdf906d3af6aa396f944e4590f
sabahatk <sabahatkhan@live.ca>
2024-04-08 22:26:52 +0000

    Week 14 Add List Lesson

---
+++ module4-add-list.py
@@ -0,0 +1,10 @@
+print("hello")
+
+name = input("name?")
+book_ratings = [7, 9, 5, 6, 8]
+book_ratings.append(4)
+print(book_ratings)
+
+book_ratings = [7, 9, 5, 6, 8]
+book_ratings.insert(1, 10)
+print(book_ratings)
