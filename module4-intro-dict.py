cfd46204a30b53ffc3ef1758b4f5442c0d98db5d
sabahatk <sabahatkhan@live.ca>
2024-04-11 23:35:11 +0000

    Week 14 Dictionary Intro Lesson

---
+++ module4-intro-dict.py
@@ -0,0 +1,39 @@
+emails = {
+    "Anne Stahl": "astahl@gmail.com",
+    "Peter Small": "peters@yandex.com",
+    "Mark Steel": "mark@steel.com"
+}
+
+print(emails["Mark Steel"])
+
+
+spanish_animals = {
+    "dog": "el perro",
+    "cat": "el gato",
+    "horse": "el caballo",
+    "bird": "el pajaro"
+}
+
+print(spanish_animals["bird"])
+
+spanish_animals = {
+    "dog": "el perro",
+    "cat": "el gato",
+    "horse": "el caballo",
+    "bird": "el pajaro",
+    "bird": "el ave"
+}
+
+print(spanish_animals) #cannot have a same key
+
+tennis_ranking = {
+    [1]: "Ashleigh Barty",
+    [2]: "Naomi Osaka"
+    [3]: "Simona Halep"
+}
+
+city_ratings = {
+    "Bangkok": [7, 4, 7, 5],
+    "Hanoi": [7, 6, 4, 5],
+    "Manila": [6, 6, 4, 4, 5]
+}
