ff7968887510b3b2589820fda9e40add29c36b68
sabahatk <sabahatkhan@live.ca>
2024-04-12 00:02:45 +0000

    Week 14 Dictionary Operators Lesson

---
+++ module4-dict-operations.py
@@ -0,0 +1,38 @@
+grades = {}
+
+grades["John"] = 'A-'
+grades["Anne"] = 'B'
+print(grades)
+
+grades["Anne"] = "A"
+print(grades)
+grades.update({"John":"A"})
+print(grades)
+
+print(len(grades))
+
+if "John" in grades:
+    print("John got:", grades["John"])
+
+del grades["John"]
+print(grades)
+
+grades = {}
+grades["John"] = "A-"
+grades["Anne"] = "B"
+
+for el in grades:
+    print(el)
+
+#print keys
+for el in grades.keys():
+    print(el)
+
+#print values
+for el in grades.values():
+    print(el)
+ 
+for person, grade in grades.items():
+    print(person, 'got', grade)
+
+
