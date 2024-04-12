ff654dca3ee1083ab5959f1b839a3e9246013f50
sabahatk <sabahatkhan@live.ca>
2024-04-12 03:32:55 +0000

    Week 14 None Lesson

---
+++ module5-none.py
@@ -0,0 +1,28 @@
+print("hello")
+len("hello")
+
+number = input("What is the number?")
+
+print_return = print("Hello, world!")
+print(print_return)
+
+x = None
+
+if x:
+    print("None is True")
+elif x is False:
+    print("None is False")
+else:
+    print("None is not True, or False, None is just None")
+
+x = None
+if x is None:
+    print("yes")
+if x == None:
+    print("it does")
+    
+def greet():
+    print("Hello!")
+
+x = greet()
+print(x)
