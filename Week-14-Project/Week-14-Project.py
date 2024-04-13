0a1edb54a09a5fe6ebe0c56e11fa97bae1b7362c
sabahatk <sabahatkhan@live.ca>
2024-04-13 17:07:01 +0000

    Cleaned up code, turned the code into a function taking in filepath parameter

--- Week-14-Project/Week-14-Project.py
+++ Week-14-Project/Week-14-Project.py
@@ -1,26 +1,24 @@
 import os
-current_files = os.listdir('.')
-#print(current_files)
-#print(os.listdir('.'))
-#print(os.getcwd())
-current_filepaths = []
 
-for file in current_files:
-    #if file.isdir()
-    #print(file)
-    #i == false:
-    #if os.path.isdir(file) == 
-    if os.path.isfile(file):
-        #print(os.path.isfile(file), file)
-        
-        filepath = os.getcwd() + '/' + file
+def print_filepaths(src_filepath = "."):
+    current_files = os.listdir(src_filepath)
+    current_filepaths = []
+    #print(src_filepath)
+    
+    for file in current_files:
+ 
+        #if os.path.isfile(file):
+            
+        if src_filepath == ".":
+            filepath = os.getcwd() + '/' + file
+        else:
+            filepath = src_filepath + file
         size = os.path.getsize(filepath)
         file_info = {"path": filepath, "size": size}
         current_filepaths.append(file_info)
-        #print(filepath, size)
-        #print(file_info)
-    #print(filepath)
-    #print(os.path.getsize(filepath))
-    #filepath = 
-    #current_filepaths.append()
-print(current_filepaths)
+
+    for file in current_filepaths:
+        print(file)
+
+
+print_filepaths('/home/ec2-user/environment/test_folder/')
