880f6fd7d0526de8a45e89ba3ae851de3356e660
sabahatk <sabahatkhan@live.ca>
2024-04-13 18:51:10 +0000

    Added a walk_files() function to recursively go through the files and print subdirectory files as well. Cleaned up code and also documented the code

--- Week-14-Project/Week-14-Project.py
+++ Week-14-Project/Week-14-Project.py
@@ -1,24 +1,63 @@
 import os
 
+#This function creates a list of dictionaries from the filepaths obtained by the walk_files() function
 def print_filepaths(src_filepath = "."):
-    current_files = os.listdir(src_filepath)
-    current_filepaths = []
-    #print(src_filepath)
+    #Recursively obtain the list of files from walk_files function
+    filepath_list = walk_files(src_filepath)
+    file_info_list = []
     
-    for file in current_files:
- 
-        #if os.path.isfile(file):
-            
-        if src_filepath == ".":
-            filepath = os.getcwd() + '/' + file
+    #iterate through each filepath in filepath_list and create
+    #dictionary items to add to file_info_list
+    for filepath in filepath_list:
+        #Fetch the size of the file for the filepath that is inputted
+        #Uses os.getcwd() function to use as the filepath if the filepath variable is equal to '.'.
+        #Otherwise uses the filepath variable instead.
+        if filepath == '.':
+            size = os.path.getsize(os.getcwd())
         else:
-            filepath = src_filepath + file
-        size = os.path.getsize(filepath)
+            size = os.path.getsize(filepath)
+        
+        #Create a dictionary containing the filepath and the size as key/pair values    
         file_info = {"path": filepath, "size": size}
-        current_filepaths.append(file_info)
-
-    for file in current_filepaths:
+        #Add the dictionary item to the file_info_list list
+        file_info_list.append(file_info)
+    
+    #iterate through each item in file_info_list and print each item
+    for file in file_info_list:
         print(file)
 
+#This function goes through all of the files and subdirectories in the file path specified by the user
+#and appends all filepaths to a list. The function then returns this list
+def walk_files(src_filepath = "."):
+    filepath_list = []
+   
+    #This for loop uses the os.walk() function to walk through the files and directories
+    #and records the filepaths of the files to a list
+    for root, dirs, files in os.walk(src_filepath):
+        
+        #iterate through the files currently obtained by os.walk() and
+        #create the filepath string for that file and add it to the filepath_list list
+        for file in files:
+            #Checks to see if the root is '.' and changes it to the correct current
+            #working directory by calling os.getcwd(). Otherwise root_path will just be the root variable value.
+            if root == '.':
+                root_path = os.getcwd() + "/"
+            else:
+                root_path = root
+            
+            #This if statement checks to see if an extra '/' character is needed to append 
+            #to the filepath or not
+            if (root_path != src_filepath) and (root != '.'):
+                filepath = root_path + "/" + file
+            else:
+                filepath = root_path + file
+            
+            #Appends filepath to filepath_list if filepath does not currently exist in filepath_list
+            if filepath not in filepath_list:
+                filepath_list.append(filepath)
+                
+    #Return filepath_list        
+    return filepath_list
 
+#Call print_filepaths function with the specified filepath
 print_filepaths('/home/ec2-user/environment/test_folder/')
